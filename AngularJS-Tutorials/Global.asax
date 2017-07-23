3246ad77c4b17efbf661af9672c3b6db21936dbe
Jonia Fraser <joniafraser@Jonia-Frasers-MacBook-Pro.local>
2017-07-22 23:36:23 -0400

    First commit of tutorial code

---
+++ AngularJS-Tutorials/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Inherits="AngularJSTutorials.Global" %>
---
+++ AngularJS-Tutorials/Global.asax.cs
@@ -0,0 +1,11 @@
+﻿using System.Web;
+
+namespace AngularJSTutorials
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+        }
+    }
+}
